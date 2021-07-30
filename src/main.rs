59205bce71863ec6ab801cb4875dfdf77299cf69
Lucas Rooyakkers <lucas.rooyakkers@protonmail.com>
2021-07-30 02:06:38 -0400

    Uzas pli malgrandan argumentparsilon por malpliigi kompiladtempon.

--- src/main.rs
+++ src/main.rs
@@ -1,25 +1,49 @@
-use rand::rngs::SmallRng;
+use pico_args::Arguments as Argumentoj;
+
+use rand::rngs::SmallRng as Hazardileto;
 use rand::seq::IteratorRandom;
 use rand::SeedableRng;
-use structopt::StructOpt;
 
-#[derive(StructOpt)]
-struct Argumentujo {
-    #[structopt(default_value = "1")]
-    /// Nombro da proverboj montri.
-    nombro: usize,
+const HELPO: &str = r#"
+UZADO:
+    proverbilo [nombro]
+
+ARGOJ:
+    <nombro>    Nombro da proverboj montri [defaŭlto: 1]
+
+FLAGOJ:
+    -h, --helpo      Printas help-informon.
+"#;
+
+fn parsu_argumentojn() -> usize {
+    let mut argumentoj = Argumentoj::from_env();
+
+    // Kontrolu ĉu la uzanto petis la helpon.
+    if argumentoj.contains(["-h", "--helpo"]) {
+        print!("{}", HELPO);
+        std::process::exit(0);
+    }
+
+    match argumentoj.opt_free_from_str() {
+        // Akiru nombron el uzanto, aŭ nur printu unu proverbon.
+        Ok(eble_nombro) => eble_nombro.unwrap_or(1),
+        // Eraro okazis.
+        Err(_) => {
+            eprintln!("eraro parsante argumento(j)n");
+            std::process::exit(1);
+        }
+    }
 }
 
 fn main() {
     // Akiri proverbojn el tekstdosiero.
     let proverbaro = include_str!("../proverbaro.txt").split('\n');
 
-    // Atingi argumentojn el la uzanto.
-    let argumentoj = Argumentujo::from_args();
+    // Akiri la nombron da proverbo printi el la uzanto.
+    let nombro = parsu_argumentojn();
 
     // Elekti hazarda(j)n proverbo(j)n.
-    let proverboj_printotaj =
-        proverbaro.choose_multiple(&mut SmallRng::from_entropy(), argumentoj.nombro);
+    let proverboj_printotaj = proverbaro.choose_multiple(&mut Hazardileto::from_entropy(), nombro);
 
     // Printi la proverbojn.
     for proverbo in proverboj_printotaj {
